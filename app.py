83b3d154af503a4151c5fb8803f188992dcbee7d
st10051449 <st10051449@vcconnect.edu.za>
2026-01-23 10:51:41 +0200

    Fix issues that are not showing how the componets look

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", active_page="dashboard")
+
+@app.route("/ui")
+def ui_showcase():
+    return render_template("ui_showcase.html", active_page="ui")
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(debug=True)
